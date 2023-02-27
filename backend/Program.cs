f83784cdbcc700bbd904a969398efe22566a920a
13angs <13angz.um@gmail.com>
2023-02-27 08:05:09 +0000

    add cors policies

--- backend/Program.cs
+++ backend/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
+IConfiguration configuration = builder.Configuration;
 
 // Add services to the container.
 
@@ -20,6 +21,17 @@ builder.Services.AddControllers(config =>
              .Json.ReferenceLoopHandling.Ignore)
                  .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver
                      = new DefaultContractResolver());
+
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy(name: configuration["CorsName"], build =>
+  {
+    build.WithOrigins(configuration["AllowedHosts"])
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+
+  });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -35,7 +47,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseCors(configuration["CorsName"]);
+
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
